0684aed73d748c0522b4909dd37632e803a1dd4a
Vreddhi Bhat <vbhat@yyz-mpd3l.hitronhub.home>
2022-06-29 00:34:58 -0400

    Updating the config

--- terraform/init.sh
+++ terraform/init.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+sudo apt update
 sudo apt install git
+
+cd /home/ubuntu
+
+sudo apt install docker.io
 git clone https://github.com/vreddhi/salt.git
+
 cd salt
+sudo docker build -t custom-nginx .
+sudo docker run --name custom-nginx_1 -d -p 80:80 custom-nginx
